ebb43239cecfd06ac19082c9c384c2926a3586cb
caesiumy <dbs2636@gmail.com>
2023-03-28 22:34:48 +0900

    feat: parseDatabaseItems 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,32 @@
 import { getDatabaseItems } from "@/cms/notionClient";
+import {
+  parseDatabaseItems,
+  ParsedDatabaseItemType,
+} from "@/utils/parseDatabaseItems";
 import { GetStaticProps } from "next";
 import React from "react";
 
-const Home = () => {
+interface HomeProps {
+  databaseItems: ParsedDatabaseItemType[];
+}
+
+const Home = ({ databaseItems }: HomeProps) => {
+  console.log("databaseItems :>> ", databaseItems);
+
   return <div>Home</div>;
 };
 
 export default Home;
 
-export const getStaticProps: GetStaticProps = async () => {
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
   if (!process.env.DATABASE_ID) throw new Error("DATABASE_ID is not defined");
   const databaseItems = await getDatabaseItems(process.env.DATABASE_ID);
 
-  console.log("databaseItems :>> ", databaseItems);
+  const parsedDatabaseItems = parseDatabaseItems(databaseItems);
 
   return {
-    props: {},
+    props: {
+      databaseItems: parsedDatabaseItems,
+    },
   };
 };
---
+++ utils/parseDatabaseItems.ts
@@ -0,0 +1,52 @@
+import { getDatabaseItems } from "@/cms/notionClient";
+import {
+  PageObjectResponse,
+  MultiSelectPropertyItemObjectResponse,
+} from "@notionhq/client/build/src/api-endpoints";
+
+export interface ParsedDatabaseItemType {
+  id: string;
+  cover: string;
+  icon: PageObjectResponse["icon"];
+  tags: MultiSelectPropertyItemObjectResponse["multi_select"];
+  published: string;
+  description: string;
+  title: string;
+}
+
+export const parseDatabaseItems = (
+  items: Awaited<ReturnType<typeof getDatabaseItems>>
+) => {
+  const parsedItems = items.reduce<ParsedDatabaseItemType[]>((acc, item) => {
+    if (!("properties" in item)) return acc;
+
+    const { id, icon, cover } = item;
+    const { 태그, 작성일, 설명, 이름 } = item.properties;
+
+    const parsedCover =
+      cover?.type === "file" ? cover.file.url : cover?.external.url ?? "";
+
+    const published = (작성일.type === "date" ? 작성일.date?.start : "") ?? "";
+
+    const description =
+      (설명.type === "rich_text" ? 설명.rich_text[0]?.plain_text : "") ?? "";
+    const title =
+      (이름.type === "title" ? 이름.title[0]?.plain_text : "") ?? "";
+
+    const tags = 태그.type === "multi_select" ? 태그.multi_select : [];
+
+    const parsedResult: ParsedDatabaseItemType = {
+      id,
+      icon,
+      cover: parsedCover,
+      published,
+      description,
+      title,
+      tags,
+    };
+
+    return [...acc, parsedResult];
+  }, []);
+
+  return parsedItems;
+};
