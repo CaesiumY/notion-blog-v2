bc15ec82fdf593067e6d7c794cd98b3120f12129
caesiumy <dbs2636@gmail.com>
2023-04-18 22:01:53 +0900

    feat: 페이지네이션 컴포넌트 추가

--- components/intro/CardSection.tsx
+++ components/intro/CardSection.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import CardList from "../card/CardList";
 import { ParsedDatabaseItemType } from "@/utils/parseDatabaseItems";
+import Pagination from "../common/Pagination";
+import { ITEMS_PER_PAGE } from "@/const/const";
 
 interface CardSectionProps {
   cardItems: ParsedDatabaseItemType[];
@@ -12,7 +14,7 @@ const CardSection = ({ cardItems }: CardSectionProps) => {
       <div className="max-w-5xl w-4/5 mx-auto flex flex-col gap-6 py-8">
         <h3 className="font-bold text-3xl">Posts</h3>
         <CardList cardItems={cardItems} />
-        {/* pagination */}
+        <Pagination totalPage={Math.ceil(cardItems.length / ITEMS_PER_PAGE)} />
       </div>
     </section>
   );
