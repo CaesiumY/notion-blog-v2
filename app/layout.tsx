a9b0d86da231a919bb17ea1ccb39492e16310eb0
caesiumy <dbs2636@gmail.com>
2023-05-09 20:57:18 +0900

    feat: app/layout 추가

---
+++ app/layout.tsx
@@ -0,0 +1,24 @@
+import React, { PropsWithChildren } from "react";
+
+import "react-notion-x/src/styles.css";
+import "prismjs/themes/prism-tomorrow.css";
+import "katex/dist/katex.min.css";
+import "pretendard/dist/web/variable/pretendardvariable.css";
+import "@/styles/globals.css";
+import "@/styles/notionStyle.scss";
+import Header from "@/components/layout/Header";
+import Footer from "@/components/layout/Footer";
+
+const RootLayout = ({ children }: PropsWithChildren) => {
+  return (
+    <html lang="ko">
+      <body>
+        <Header />
+        <main>{children}</main>
+        <Footer />
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  experimental: {
+    appDir: true,
+  },
   images: {
     domains: ["www.notion.so", "s3.us-west-2.amazonaws.com"],
   },
